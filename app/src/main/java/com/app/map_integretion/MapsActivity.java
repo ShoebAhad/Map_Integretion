1e2f1fa3186682a223cd4c4fa9fdd73383f555ed
ShoebAhad <ahadbinislam1111@gmail.com>
2022-09-30 01:58:12 +0600

    MapIntegretion first commit

---
+++ app/src/main/java/com/app/map_integretion/MapsActivity.java
@@ -0,0 +1,41 @@
+  package com.app.map_integretion;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.app.map_integretion.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng Dhaka = new LatLng(23.7808871, 90.2788491);
+        mMap.addMarker(new MarkerOptions().position(Dhaka).title("Marker in Dhaka"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Dhaka));
+    }
+}
